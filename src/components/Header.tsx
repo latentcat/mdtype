8d5bf9a034aa2ac9fc4c904c1772a1514acd4e0e
ciaochaos <1272777550@qq.com>
2024-04-02 16:11:06 +0800

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,15 @@ import {HeaderToolbar} from "@/components/HeaderToolbar";
 import {DarkModeToggle} from "@/components/DarkModeToggle";
 import {Button} from "@/components/ui/button";
 import {PostButton} from "@/components/PostButton";
+import {MdniceLogo} from "@/components/Logos";
 
 
 export function Header() {
   return (
-    <div className="flex items-center gap-4 h-12 px-6">
+    <div className="flex items-center gap-4 h-12 px-2">
       <div className="grow flex items-center gap-4">
-        <ArticleSelector />
+        <MdniceLogo className="h-5 ml-2"/>
+        {/*<ArticleSelector />*/}
         <MenubarDemo />
       </div>
       <div className="flex items-center gap-4">
