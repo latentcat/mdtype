02f56da3045ad5ac678744e64130696ce7089922
CPunisher <1343316114@qq.com>
2024-04-02 17:41:56 +0800

    Render html and css in PreviewView

--- src/lib/store/editor.ts
+++ src/lib/store/editor.ts
@@ -1,5 +1,18 @@
 import { atom } from "jotai";
+import rehypeSanitize from "rehype-sanitize";
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
 
 export const markdownAtom = atom("");
 export const cssAtom = atom("");
-export const htmlAtom = atom("");
+export const htmlAtom = atom(async (get) => {
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeSanitize)
+    .use(rehypeStringify)
+    .process(get(markdownAtom));
+  return file.toString();
+});
